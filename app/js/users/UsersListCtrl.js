122efe89b4aebf2a0ecd27c061cd365125ae1b19
Carlos Cessa <carlos@pixative.com>
2016-03-21 01:48:55 -0600

    Users Search
    - Added support to search users in the application by name
    - Included markup, styles and language resources for the new UI

--- app/js/users/UsersListCtrl.js
+++ app/js/users/UsersListCtrl.js
@@ -4,8 +4,10 @@ define( function () {
     return function ( $scope, $stateParams, Users ) {
         $scope.page     = 1;
         $scope.per_page = 10;
+        $scope.name     = '';
         $scope.query    = function () {
             $scope.users    = Users.query({
+                name        : $scope.name,
                 page        : $scope.page,
                 per_page    : $scope.per_page,
                 type        : $stateParams.type
