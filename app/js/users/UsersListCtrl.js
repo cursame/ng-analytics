49f6f08198a9a39c02fb29fe5c7380811f9e9c33
Carlos Cessa <carlos@pixative.com>
2016-03-21 00:05:29 -0600

    Users Pagination
    - Added pagination support to the users list controller and view

--- app/js/users/UsersListCtrl.js
+++ app/js/users/UsersListCtrl.js
@@ -2,8 +2,20 @@
 
 define( function () {
     return function ( $scope, $stateParams, Users ) {
-        $scope.users    = Users.query({
-            type        : $stateParams.type
+        $scope.page     = 1;
+        $scope.per_page = 10;
+        $scope.query    = function () {
+            $scope.users    = Users.query({
+                page        : $scope.page,
+                per_page    : $scope.per_page,
+                type        : $stateParams.type
+            });
+        };
+
+        $scope.$on( Users.getEvent( 'QUERIED' ), function () {
+            $scope.total    = Users.getTotal();
         });
+
+        $scope.query();
     };
 });
