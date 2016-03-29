b528fa0cb9fb762e441b5da9d428b48640393c3d
Carlos Cessa <carlos@pixative.com>
2016-03-29 16:28:57 -0600

    User's Profile
    - Modified user's details view to include the datepicker module

--- app/js/users/UsersDetailsCtrl.js
+++ app/js/users/UsersDetailsCtrl.js
@@ -3,5 +3,8 @@
 define( function () {
     return function ( $scope, $stateParams, Users ) {
         $scope.user     = Users.get( $stateParams.id );
+        $scope.dateOpts = {
+            showWeeks   : false
+        };
     };
 });
