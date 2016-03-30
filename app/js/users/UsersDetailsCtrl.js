3c0704742376162d471fa4cba7ac20835e31798f
Carlos Cessa <carlos@pixative.com>
2016-03-30 13:42:04 -0600

    Users Details Calendar
    - Added functionality for the user's details view calendar to set the
    start and end of the week for the selected date

--- app/js/users/UsersDetailsCtrl.js
+++ app/js/users/UsersDetailsCtrl.js
@@ -2,9 +2,24 @@
 
 define( function () {
     return function ( $scope, $stateParams, Users ) {
+        var start       = moment().startOf( 'week' ).toDate(),
+            end         = moment().endOf( 'week' ).toDate(),
+            graph       = function () {
+                
+            };
+
         $scope.user     = Users.get( $stateParams.id );
         $scope.dateOpts = {
             showWeeks   : false
         };
+
+        $scope.$watch( 'date', function ( date ) {
+            if ( date ) {
+                start   = moment( $scope.date ).startOf( 'week' ).toDate();
+                end     = moment( $scope.date ).endOf( 'week' ).toDate();
+            }
+
+            graph();
+        });
     };
 });
