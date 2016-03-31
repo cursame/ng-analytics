7dc03bf171ac3d513b2ee0c19e4d2f4369f44c30
Carlos Cessa <carlos@pixative.com>
2016-03-31 01:41:35 -0600

    Logins Graph
    - Added logins graph in the user's profile view
    - Included styles and locales for the added UI

--- app/js/users/UsersDetailsCtrl.js
+++ app/js/users/UsersDetailsCtrl.js
@@ -1,9 +1,23 @@
 'use strict';
 
 define( function () {
-    return function ( $scope, $stateParams, Users, Logins ) {
+    return function ( $scope, $stateParams, $translate, Users, Logins ) {
         var start       = moment().startOf( 'week' ).toDate(),
             end         = moment().endOf( 'week' ).toDate(),
+            months      = [
+                $translate.instant( 'month.january' ),
+                $translate.instant( 'month.february' ),
+                $translate.instant( 'month.march' ),
+                $translate.instant( 'month.april' ),
+                $translate.instant( 'month.may' ),
+                $translate.instant( 'month.june' ),
+                $translate.instant( 'month.july' ),
+                $translate.instant( 'month.august' ),
+                $translate.instant( 'month.september' ),
+                $translate.instant( 'month.october' ),
+                $translate.instant( 'month.november' ),
+                $translate.instant( 'month.december' )
+            ],
             graph       = function () {
                 Logins.query({
                     $and        : [
@@ -20,9 +34,33 @@ define( function () {
                     ],
                     aggregate   : 'date:day',
                     user        : $stateParams.id
+                }).$promise.then( function ( data ) {
+                    var date        = start,
+                        k           = 1;
+                    $scope.labels   = [];
+                    $scope.data     = [ [] ];
+
+                    for ( var  i = 0; i < 7; i++ ) {
+                        var label   = ( i == 0 ) ? new Date( date.setDate( start.getDate() ) ) : new Date( date.setDate( start.getDate() + 1 ) ),
+                            day     = label.getDate();
+                        $scope.labels.push( day + ' ' + months[ label.getMonth() ] );
+
+                        $scope.data[0][i]   = 0;
+                        for ( var j = 0; j < data.length; j++ ) {
+                            if ( day == data[j]._id ) {
+                                $scope.data[0][i]   = data[j].count;
+                                break;
+                            }
+                        }
+                    }
                 });
             };
 
+        $scope.options  = {
+            responsive          : true,
+            barValueSpacing     : 7,
+            barStrokeWidth      : 1
+        };
         $scope.user     = Users.get( $stateParams.id );
         $scope.dateOpts = {
             showWeeks   : false
