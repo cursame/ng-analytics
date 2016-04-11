c29d11cb1bdae08b90b77835acf2eb94bcc8be24
Carlos Cessa <carlos@pixative.com>
2016-04-11 18:24:05 -0500

    Code Refactor
    - Modified users details and analytics general view to use the new
    logins directive and avoid code repetition

--- app/js/analytics/AnalyticsGeneralCtrl.js
+++ app/js/analytics/AnalyticsGeneralCtrl.js
@@ -1,76 +1,7 @@
 'use strict';
 
 define( function () {
-    return function ( $scope, $translate, Logins ) {
-        var start       = moment().startOf( 'week' ).toDate(),
-            end         = moment().endOf( 'week' ).toDate(),
-            months      = [
-                $translate.instant( 'month.january' ),
-                $translate.instant( 'month.february' ),
-                $translate.instant( 'month.march' ),
-                $translate.instant( 'month.april' ),
-                $translate.instant( 'month.may' ),
-                $translate.instant( 'month.june' ),
-                $translate.instant( 'month.july' ),
-                $translate.instant( 'month.august' ),
-                $translate.instant( 'month.september' ),
-                $translate.instant( 'month.october' ),
-                $translate.instant( 'month.november' ),
-                $translate.instant( 'month.december' )
-            ],
-            graph       = function () {
-                Logins.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $gte    : start
-                            }
-                        },
-                        {
-                            date        : {
-                                $lte    : end
-                            }
-                        }
-                    ],
-                    aggregate   : 'date:day'
-                }).$promise.then( function ( data ) {
-                    var date        = start,
-                        k           = 1;
-                    $scope.labels   = [];
-                    $scope.data     = [ [] ];
-
-                    for ( var  i = 0; i < 7; i++ ) {
-                        var label   = ( i == 0 ) ? new Date( date.setDate( start.getDate() ) ) : new Date( date.setDate( start.getDate() + 1 ) ),
-                            day     = label.getDate();
-                        $scope.labels.push( day + ' ' + months[ label.getMonth() ] );
-
-                        $scope.data[0][i]   = 0;
-                        for ( var j = 0; j < data.length; j++ ) {
-                            if ( day == data[j]._id ) {
-                                $scope.data[0][i]   = data[j].count;
-                                break;
-                            }
-                        }
-                    }
-                });
-            };
-
-        $scope.options          = {
-            responsive          : true,
-            barValueSpacing     : 7,
-            barStrokeWidth      : 1
-        };
-        $scope.dateOpts         = {
-            showWeeks   : false
-        };
-
-        $scope.$watch( 'date', function ( date ) {
-            if ( date ) {
-                start   = moment( $scope.date ).startOf( 'week' ).toDate();
-                end     = moment( $scope.date ).endOf( 'week' ).toDate();
-            }
-
-            graph();
-        });
+    return function ( $scope ) {
+        
     };
 });
--- app/js/users/UsersDetailsCtrl.js
+++ app/js/users/UsersDetailsCtrl.js
@@ -2,80 +2,9 @@
 
 define( function () {
     return function ( $scope, $stateParams, $translate, events, Users, Logins, Activities, Courses, Assignments, Discussions, Comments, Questionaries, Files ) {
-        var start       = moment().startOf( 'week' ).toDate(),
-            end         = moment().endOf( 'week' ).toDate(),
-            months      = [
-                $translate.instant( 'month.january' ),
-                $translate.instant( 'month.february' ),
-                $translate.instant( 'month.march' ),
-                $translate.instant( 'month.april' ),
-                $translate.instant( 'month.may' ),
-                $translate.instant( 'month.june' ),
-                $translate.instant( 'month.july' ),
-                $translate.instant( 'month.august' ),
-                $translate.instant( 'month.september' ),
-                $translate.instant( 'month.october' ),
-                $translate.instant( 'month.november' ),
-                $translate.instant( 'month.december' )
-            ],
-            graph       = function () {
-                Logins.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $gte    : start
-                            }
-                        },
-                        {
-                            date        : {
-                                $lte    : end
-                            }
-                        }
-                    ],
-                    aggregate   : 'date:day',
-                    user        : $stateParams.id
-                }).$promise.then( function ( data ) {
-                    var date        = start,
-                        k           = 1;
-                    $scope.labels   = [];
-                    $scope.data     = [ [] ];
-
-                    for ( var  i = 0; i < 7; i++ ) {
-                        var label   = ( i == 0 ) ? new Date( date.setDate( start.getDate() ) ) : new Date( date.setDate( start.getDate() + 1 ) ),
-                            day     = label.getDate();
-                        $scope.labels.push( day + ' ' + months[ label.getMonth() ] );
-
-                        $scope.data[0][i]   = 0;
-                        for ( var j = 0; j < data.length; j++ ) {
-                            if ( day == data[j]._id ) {
-                                $scope.data[0][i]   = data[j].count;
-                                break;
-                            }
-                        }
-                    }
-                });
-            };
-
-        $scope.options          = {
-            responsive          : true,
-            barValueSpacing     : 7,
-            barStrokeWidth      : 1
-        };
         $scope.user             = Users.get( $stateParams.id );
-        $scope.dateOpts         = {
-            showWeeks   : false
-        };
         $scope.page             = 1;
         $scope.per_page         = 10;
-
-        $scope.$watch( 'date', function ( date ) {
-            if ( date ) {
-                start   = moment( $scope.date ).startOf( 'week' ).toDate();
-                end     = moment( $scope.date ).endOf( 'week' ).toDate();
-            }
-
-            graph();
-        });
 
         $scope.$on( Activities.getEvent( 'QUERIED' ), function () {
             $scope.totalActivities  = Activities.getTotal();
@@ -227,7 +156,7 @@ define( function () {
                 });
             }
         });
-        $scope.$on( Users.getEvent('RETRIEVED' ), function () {
+        $scope.$on( Users.getEvent( 'RETRIEVED' ), function () {
             switch ( $scope.user.type ) {
                 case 1 :
                     $scope.queryActivities  = function () {
