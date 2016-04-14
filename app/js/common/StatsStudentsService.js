d375f73227f4f30046efbbd1caea901049a4e4c1
Carlos Cessa <carlos@pixative.com>
2016-04-14 15:39:51 -0500

    Students Statistical Service
    - Refactor students statistical service to return an unique promise
    object for every invocation

--- app/js/common/StatsStudentsService.js
+++ app/js/common/StatsStudentsService.js
@@ -2,220 +2,219 @@
 
 define( function () {
     return function ( $q, Courses, Assignments, Discussions, Questionaries ) {
-        return {
-            _courses            : [],
-
-            _data               : {
+        function StudentsStats ( user, course ) {
+            this._course    = course;
+            this._courses   = [];
+            this._data      = {
                 assignments         : false,
                 assignmentsSolved   : false,
                 discussions         : false,
                 discussionsSolved   : false,
                 questionaries       : false,
                 questionariesSolved : false
-            },
+            };
+            this._deferred  = $q.defer();
+            this._filters   = [];
+            this._response  = {};
+            this._user      = user;
+        };
 
-            _deferred           : null,
+        StudentsStats.prototype._check              = function () {
+            var that    = this;
+            if ( this._data.assignments !== false && this._data.assignmentsSolved !== false &&
+                 this._data.discussions !== false && this._data.discussionsSolved !== false &&
+                 this._data.questionaries !== false && this._data.questionariesSolved !== false ) {
+                this._deferred.resolve( this._data );
+            }
+        };
 
-            _filters            : [],
+        StudentsStats.prototype._getAssignments     = function () {
+            var that    = this;
 
-            _response           : {},
+            Assignments.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._courses
+                    }
+                ],
+                $or         : this._courses,
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.assignments          = Assignments.getTotal();
+                that._check();
+            });
+            Assignments.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._filters
+                    }
+                ],
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.assignmentsSolved    = Assignments.getTotal();
+                that._check();
+            });
+        };
 
-            _user               : null,
+        StudentsStats.prototype._getDiscussions     = function () {
+            var that    = this;
 
-            _check              : function () {
-                var that    = this;
-                if ( this._data.assignments !== false && this._data.assignmentsSolved !== false &&
-                     this._data.discussions !== false && this._data.discussionsSolved !== false &&
-                     this._data.questionaries !== false && this._data.questionariesSolved !== false ) {
-                    this._deferred.resolve( this._data );
+            Discussions.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._courses
+                    }
+                ],
+                $or         : this._courses,
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.discussions          = Discussions.getTotal();
+                that._check();
+            });
+            Discussions.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._filters
+                    }
+                ],
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.discussionsSolved    = Discussions.getTotal();
+                that._check();
+            });
+        };
+
+        StudentsStats.prototype._getQuestionaries   = function () {
+            var that    = this;
+
+            Questionaries.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._courses
+                    }
+                ],
+                $or         : this._courses,
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.questionaries        = Questionaries.getTotal();
+                that._check();
+            });
+            Questionaries.query({
+                $and        : [
+                    {
+                        date        : {
+                            $lte    : moment().endOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        date        : {
+                            $gte    : moment().startOf( 'month' ).toDate()
+                        }
+                    },
+                    {
+                        $or         : this._filters
+                    }
+                ],
+                per_page    : 1
+            }).$promise.then( function ( data ) {
+                that._data.questionariesSolved  = Questionaries.getTotal();
+                that._check();
+            });
+        };
+
+        StudentsStats.prototype.getResponse         = function () {
+            var that        = this;
+
+            Courses.query({
+                limit       : 99999,
+                students    : this._user
+            }).$promise.then( function ( data ) {
+                for ( var i = 0; i < data.length; i++ ) {
+                    that._courses.push({
+                        course  : data[i]._id
+                    });
+
+                    that._filters.push({
+                        $and    : [
+                            {
+                                course      : data[i]._id
+                            },
+                            {
+                                students    : that._user
+                            }
+                        ]
+                    });
                 }
-            },
 
-            _getAssignments     : function () {
-                var that    = this;
+                that._getAssignments();
+                that._getDiscussions();
+                that._getQuestionaries();
+            });
 
-                Assignments.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._courses
-                        }
-                    ],
-                    $or         : this._courses,
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.assignments          = Assignments.getTotal();
-                    that._check();
-                });
-                Assignments.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._filters
-                        }
-                    ],
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.assignmentsSolved    = Assignments.getTotal();
-                    that._check();
-                });
-            },
+            this._response.promise      = this._deferred.promise;
 
-            _getDiscussions     : function () {
-                var that    = this;
+            this._response.promise.then( function ( data ) {
+                for ( var key in data ) {
+                    that._response[key] = data[key];
+                }
+            });
 
-                Discussions.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._courses
-                        }
-                    ],
-                    $or         : this._courses,
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.discussions          = Discussions.getTotal();
-                    that._check();
-                });
-                Discussions.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._filters
-                        }
-                    ],
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.discussionsSolved    = Discussions.getTotal();
-                    that._check();
-                });
-            },
+            return this._response;
+        };
 
-            _getQuestionaries   : function () {
-                var that    = this;
-
-                Questionaries.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._courses
-                        }
-                    ],
-                    $or         : this._courses,
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.questionaries        = Questionaries.getTotal();
-                    that._check();
-                });
-                Questionaries.query({
-                    $and        : [
-                        {
-                            date        : {
-                                $lte    : moment().endOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            date        : {
-                                $gte    : moment().startOf( 'month' ).toDate()
-                            }
-                        },
-                        {
-                            $or         : this._filters
-                        }
-                    ],
-                    per_page    : 1
-                }).$promise.then( function ( data ) {
-                    that._data.questionariesSolved  = Questionaries.getTotal();
-                    that._check();
-                });
-            },
-
-            getStats            : function ( user ) {
-                var that        = this;
-
-                this._deferred  = $q.defer();
-                this._user      = user;
-
-                Courses.query({
-                    limit       : 99999,
-                    students    : user
-                }).$promise.then( function ( data ) {
-                    for ( var i = 0; i < data.length; i++ ) {
-                        that._courses.push({
-                            course  : data[i]._id
-                        });
-
-                        that._filters.push({
-                            $and    : [
-                                {
-                                    course      : data[i]._id
-                                },
-                                {
-                                    students    : user
-                                }
-                            ]
-                        });
-                    }
-
-                    that._getAssignments();
-                    that._getDiscussions();
-                    that._getQuestionaries();
-                });
-
-                this._response.promise      = this._deferred.promise;
-
-                this._response.promise.then( function ( data ) {
-                    for ( var key in data ) {
-                        that._response[key] = data[key];
-                    }
-                });
-
-                return this._response;
+        return {
+            getStats            : function ( user, course ) {
+                return new StudentsStats( user, course ).getResponse();
             }
         };
     };
